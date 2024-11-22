4535d851ad1f93670df6190bbea787b8356e705e
Simbolon105 <dewantha.105@gmail.com>
2024-11-22 21:41:50 +0700

    feat: 70% dashboard admin page

--- src/views/DashboardAdmin.jsx
+++ src/views/DashboardAdmin.jsx
@@ -5,7 +5,7 @@ import { GiBrokenTablet } from "react-icons/gi";
 
 const DashboardAdmin = () => {
   return (
-    <div className="flex w-full h-full flex-row ">
+    <div className="flex w-full h-full flex-row font-poppins">
       <section className="w-full basis-1/5 h-screen p-5 ">
         <div className="flex gap-2">
           <img
@@ -18,25 +18,25 @@ const DashboardAdmin = () => {
           </h1>
         </div>
         <div className="flex flex-col mt-5 gap-3">
-          <button className="text-start bg-blue-800 p-2 text-white rounded">
-            <img src="" alt="" />
-            <span>Dashboard</span>
+          <button className="flex text-star hover:bg-[#3852A9] px-3 py-2 hover:text-white  gap-2 rounded ">
+            <RiAlignItemBottomLine size={30} className="" />
+            <span className="align-center">Dashboard</span>
           </button>
-          <button className="text-start  bg-blue-800 p-2 text-white rounded">
+          <button className="text-start hover:bg-[#3852A9] px-3 py-2 hover:text-white rounded">
             <img src="" alt="" />
             <span>Data Peminjaman</span>
           </button>
-          <button className="text-start  bg-blue-800 p-2 text-white rounded">
+          <button className="text-start hover:bg-[#3852A9] px-3 py-2 hover:text-white rounded">
             <img src="" alt="" />
             <span>Manajemen Barang</span>
           </button>
-          <button className="text-start  bg-blue-800 p-2 text-white rounded">
+          <button className="text-start hover:bg-[#3852A9] px-3 py-2 hover:text-white rounded">
             <img src="" alt="" />
             <span>Logout</span>
           </button>
         </div>
       </section>
-      <section className="bg-blue-100 w-full basis-4/5">
+      <section className="bg-[#EFF0F6] w-full basis-4/5">
         <nav className="flex bg-white p-3 gap-3 items-center justify-end pr-10">
           <h3 className="">Kurnia Jaya</h3>
           <img
@@ -45,75 +45,106 @@ const DashboardAdmin = () => {
             className="w-14 h-14 rounded-full"
           />
         </nav>
-        <section>
-          <div className="flex mt-10 gap-10 justify-around ml-7 mr-8">
-            <div className="bg-white rounded-md px-5 py-9 border">
-              <div className="flex items-center">
+        <div className="p-7">
+          <section className="grid grid-flow-col gap-6  justify-stretch">
+            <div className="flex bg-white rounded-xl gap-8 px-5 py-9 border">
+              <div className="flex items-center gap-5">
                 <RiAlignItemBottomLine
                   size={50}
-                  className="bg-[#D398E7] text-white mr-2 rounded-xl"
+                  className="bg-[#98A8E7] p-2 text-white rounded-2xl"
                 />
                 <div>
-                  <h2 className="font-poppins">Barang Tersedia</h2>
-                  <p className="text-4xl">300</p>
+                  <p className="font-poppins opacity-40 text-xs">
+                    Barang Tersedia
+                  </p>
+                  <h2 className="text-5xl font-medium">300</h2>
                 </div>
               </div>
             </div>
-            <div className="bg-white rounded-md px-5 py-9 border">
-              <div className="flex items-center">
+            <div className="bg-white rounded-xl px-5 py-9 border">
+              <div className="flex items-center gap-5">
                 <RiBringToFront
                   size={50}
-                  className="bg-[#D398E7] text-white mr-2 rounded-xl"
+                  className="bg-[#D398E7] p-2 text-white rounded-2xl"
                 />
                 <div>
-                  <h2 className="font-poppins">Barang Dipinjam</h2>
-                  <p className="text-4xl">80</p>
+                  <p className="font-poppins opacity-40 text-xs">
+                    Barang Dipinjam
+                  </p>
+                  <h2 className="text-5xl font-medium">80</h2>
                 </div>
               </div>
             </div>
-            <div className="bg-white rounded-md px-5 py-9 border">
-              <div className="flex items-center">
+            <div className="bg-white rounded-xl px-5 py-9 border">
+              <div className="flex items-center gap-5">
                 <GrVmMaintenance
                   size={50}
-                  className="bg-[#D398E7] text-white mr-2 rounded-xl"
+                  className="bg-[#FF9E48] p-2 text-white rounded-2xl"
                 />
                 <div>
-                  <h2 className="font-poppins">Barang Dalam Perbaikan</h2>
-                  <p className="text-4xl">70</p>
+                  <p className="font-poppins opacity-40 text-xs">
+                    Barang Dalam Perbaikan
+                  </p>
+                  <h2 className="text-5xl font-medium">70</h2>
                 </div>
               </div>
             </div>
-            <div className="bg-white rounded-md px-5 py-9 border">
-              <div className="flex items-center">
+            <div className="bg-white rounded-xl px-5 py-9 border">
+              <div className="flex items-center gap-5">
                 <GiBrokenTablet
                   size={50}
-                  className="bg-[#D398E7] text-white mr-2 rounded-xl"
+                  className="bg-[#FF4646] p-2 text-white rounded-2xl"
                 />
                 <div>
-                  <h2 className="font-poppins">Barang Rusak</h2>
-                  <p className="text-4xl">500</p>
+                  <p className="font-poppins opacity-40 text-xs">
+                    Barang Rusak
+                  </p>
+                  <h2 className="text-5xl font-medium">500</h2>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-        <section className="flex ml-7 mt-2">
-          <div className="">
-            <div className="w-auto h-auto mt-10">
-              <h1 className="text-black font-poppins font-semibold text-lg">
-                Rekapitulasi Bulanan
-              </h1>
+          </section>
+          <section className="flex mt-4 gap-6">
+            <div className="bg-white p-5 h-80 rounded-xl border basis-4/5">
+              <h6 className="font-semibold text-xs">Rekapitulasi Bulanan</h6>
             </div>
-            <div className="bg-gray-500 w-auto h-auto mr-7">
-              <h1>grafik</h1>
-            </div>
-            <div className="flex flex-col bg-black">
-              <div>
-                <h1>Detail</h1>
+            <div className="bg-white p-5 h-80 rounded-xl border basis-1/4 ">
+              <h6 className="font-semibold text-xs">Barang Sedang Dipinjam</h6>
+              <div className="flex flex-col gap-3 mt-4">
+                <div className="flex bg-[#3852A9] text-white rounded-md p-2 justify-between items-center">
+                  <h6 className="">Laptop</h6>
+                  <button className="bg-[#FF9E48] hover:bg-[#e2781c] text-white rounded-xl px-5 ">
+                    Detail
+                  </button>
+                </div>
+                <div className="flex bg-[#3852A9] text-white rounded-md p-2 justify-between items-center">
+                  <h6>Proyektor</h6>
+                  <button className="bg-[#FF9E48] hover:bg-[#e2781c] text-white rounded-xl px-5 ">
+                    Detail
+                  </button>
+                </div>
+                <div className="flex bg-[#3852A9] text-white rounded-md p-2 justify-between items-center">
+                  <h6>Kabel</h6>
+                  <button className="bg-[#FF9E48] hover:bg-[#e2781c] text-white rounded-xl px-5 ">
+                    Detail
+                  </button>
+                </div>
+                <div className="flex bg-[#3852A9] text-white rounded-md p-2 justify-between items-center">
+                  <h6>Speaker</h6>
+                  <button className="bg-[#FF9E48] hover:bg-[#e2781c] text-white rounded-xl px-5 ">
+                    Detail
+                  </button>
+                </div>
+                <div className="flex bg-[#3852A9] text-white rounded-md p-2 justify-between items-center">
+                  <h6>Mouse</h6>
+                  <button className="bg-[#FF9E48] hover:bg-[#e2781c] text-white rounded-xl px-5 ">
+                    Detail
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </section>
     </div>
   );
