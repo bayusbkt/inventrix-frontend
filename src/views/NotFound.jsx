5c2e16a277e79f413d302fd2f3714c1a94f7ab3d
Simbolon105 <dewantha.105@gmail.com>
2024-12-03 20:43:14 +0700

    feat: NotFound page

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col text-center">
+        <h1 className="text-primary font-semibold text-2xl sm:text-3xl mt-6 font-poppins">
+          404
+        </h1>
+        <p className="mt-5">Halaman Tidak Ditemukan</p>
+        <div className="flex justify-center mt-4">
+          <button
+            type="Kembali"
+            className="text-white bg-primary rounded-lg py-2 px-6 hover:bg-cyan-500 transition-colors text-sm font-medium"
+            onClick={handleBack}
+          >
+            Kembali
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
